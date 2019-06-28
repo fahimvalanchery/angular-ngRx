78cfab6d056330dbd833c3bc3232ffd0fe8a72ae
fahim <fahimvalanchery@gmail.com>
2019-06-28 13:05:36 +0530

    import @ngrx/store and our reduce

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,22 @@
-import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
+import { BrowserModule } from "@angular/platform-browser";
+import { NgModule } from "@angular/core";
 
-import { AppComponent } from './app.component';
+import { AppComponent } from "./app.component";
+
+// import @ngrx/store and our reduce
+import { StoreModule } from "@ngrx/store";
+import { reducer } from "./store/reducers/student.reducer";
 
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
+  declarations: [AppComponent],
   imports: [
-    BrowserModule
+    BrowserModule,
+    //add store module
+    StoreModule.forRoot({
+      student: reducer
+    })
   ],
   providers: [],
   bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {}
