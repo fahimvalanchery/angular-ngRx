a9107674f8b04d3ba4474bc6081683ab3aaba303
fahim <fahimvalanchery@gmail.com>
2019-06-28 13:06:28 +0530

    ng g c ViewStudents

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,9 +6,10 @@ import { AppComponent } from "./app.component";
 // import @ngrx/store and our reduce
 import { StoreModule } from "@ngrx/store";
 import { reducer } from "./store/reducers/student.reducer";
+import { ViewStudentsComponent } from './view-students/view-students.component';
 
 @NgModule({
-  declarations: [AppComponent],
+  declarations: [AppComponent, ViewStudentsComponent],
   imports: [
     BrowserModule,
     //add store module
---
+++ src/app/view-students/view-students.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-view-students',
+  templateUrl: './view-students.component.html',
+  styleUrls: ['./view-students.component.css']
+})
+export class ViewStudentsComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
