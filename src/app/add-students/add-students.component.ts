5bb9afdec371f349a89bd1ba1d874b75a48ed0d7
fahim <fahimvalanchery@gmail.com>
2019-06-28 13:19:31 +0530

    Writing to Ngrx Store

--- src/app/add-students/add-students.component.ts
+++ src/app/add-students/add-students.component.ts
@@ -1,15 +1,23 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit } from "@angular/core";
+
+// import
+import { Store } from "@ngrx/store";
+import { AppState } from "../store/app.state";
+import { Student } from "../store/models/student.model";
+import { Observable } from "rxjs";
+import * as StudentActions from "../store/actions/student.action";
 
 @Component({
-  selector: 'app-add-students',
-  templateUrl: './add-students.component.html',
-  styleUrls: ['./add-students.component.css']
+  selector: "app-add-students",
+  templateUrl: "./add-students.component.html",
+  styleUrls: ["./add-students.component.css"]
 })
 export class AddStudentsComponent implements OnInit {
+  constructor(private store: Store<AppState>) {}
 
-  constructor() { }
-
-  ngOnInit() {
+  addStudent(name) {
+    this.store.dispatch(new StudentActions.AddStudent({ name: name }));
   }
 
+  ngOnInit() {}
 }
