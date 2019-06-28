39a43d6a835b5020bcce0dc756b6ea6504cd08fd
fahim <fahimvalanchery@gmail.com>
2019-06-28 12:46:58 +0530

    Define Type of action

--- src/app/store/actions/student.action.ts
+++ src/app/store/actions/student.action.ts
@@ -2,3 +2,7 @@
 
 import { Action } from "@ngrx/store";
 import { Student } from "../models/student.model";
+
+// Define Type of action, which is in the form of a string constant
+export const ADD_STUDENT = "[STUDENT] Add";
+export const REMOVE_STUDENT = "[STUDENT] Remove";
