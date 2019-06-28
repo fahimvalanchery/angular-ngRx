a9a934490dbf42b04a093d1fd576b1e1e0465444
fahim <fahimvalanchery@gmail.com>
2019-06-28 12:57:38 +0530

    Creating actual reducer

--- src/app/store/reducers/student.reducer.ts
+++ src/app/store/reducers/student.reducer.ts
@@ -1,7 +1,17 @@
 import { Student } from "../models/student.model";
+import * as StudentActions from "../actions/student.action";
 
 // Define our initial State or default state
 
 const initialState: Student = {
   name: "Test name"
 };
+
+//Actual reducer,  It takes in a state, which we're defining as a Student type and we've optionally bound it to initialState
+
+export function reducer(
+  state: Student[] = [initialState],
+  action: StudentActions.Actions
+) {
+  //
+}
