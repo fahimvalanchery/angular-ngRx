92d79c750b268161262b3253d4c392f21de7ac9f
fahim <fahimvalanchery@gmail.com>
2019-06-28 12:50:13 +0530

    Creating class for each Action

--- src/app/store/actions/student.action.ts
+++ src/app/store/actions/student.action.ts
@@ -6,3 +6,16 @@ import { Student } from "../models/student.model";
 // Define Type of action, which is in the form of a string constant
 export const ADD_STUDENT = "[STUDENT] Add";
 export const REMOVE_STUDENT = "[STUDENT] Remove";
+
+//  Creating class for each Action with a constructor that allo us to pass in the payload
+
+export class AddStudent implements Action {
+  readonly type = ADD_STUDENT;
+
+  constructor(public payload: Student) {}
+}
+
+export class RemoveStudent implements Action {
+  readonly type = REMOVE_STUDENT;
+  constructor(public payload: number) {}
+}
